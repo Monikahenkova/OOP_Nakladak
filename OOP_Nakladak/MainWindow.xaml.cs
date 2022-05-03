2abc3fbbcc7b5588e2dfa3927f682d3cc87dd0b0
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2022-05-03 09:00:27 +0200

    Přidejte soubory projektu.

---
+++ OOP_Nakladak/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Nakladak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        nakladak Volvo;
+        nakladak Fiat;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Volvo = new nakladak();
+            Volvo.Znacka = "Volvo";
+
+            Fiat = new nakladak();
+            Fiat.Znacka = "Fiat";
+        }
+
+        public void Zobraz(nakladak nakladak, )
+    }
+}
---
+++ OOP_Nakladak/nakladak.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nakladak
+{
+    public class nakladak
+    {
+        public string Znacka { get; set; }
+        public int Nosnost { get; set; }
+        public int Nalozit { get; set; }
+        public int Objemnadrze { get; set; }
+        public int Spotreba { get; set; }
+        public int Palivo { get; set; }
+        public int Vzdalenost { get; set; }
+
+        
+
+        public nakladak()
+        {
+            Nosnost = 0;
+            Nalozit = 0;
+            Objemnadrze = 0;
+            Spotreba = 0;
+            Palivo = 0;
+            Vzdalenost = 0;
+        }
+
+
+
+
+
+
+    }
+}
